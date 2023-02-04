31ab0016d06ca81ac5259b987e9dbb27388b92fd
Lucas Machado <111795994+lucasmsdev@users.noreply.github.com>
2023-02-03 22:14:27 -0300

    Add files via upload

---
+++ python/a.py
@@ -0,0 +1,12 @@
+input1 = float(input("Digite o numero 1: \n") )
+input2 = float(input("Digite o numero 2: \n"))
+input3 = float(input("Digite o numero 3: \n"))
+
+if input1 > input2 and input1 >input3:
+    print("Numéro 1 é maior!")
+elif input2 > input3 and input2> input1:
+    print("Numero 2 é maior!")
+elif input3 > input1 and input3 >input2:
+    print("Numero 3 é maior!")
+
+    
